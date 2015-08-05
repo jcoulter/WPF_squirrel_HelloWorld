1b1696dc2ef792a096c1e32afbe2c74e148dc4e2
Jacob Coulter <jacob.coulter@leandogsoftware.com>
2015-08-05 14:23:43 -0400

    Adding restart - but it starts the old exe instead of the new one

--- HelloWorld/HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,31 +25,44 @@ namespace HelloWorld {
     /// </summary>
     public partial class MainWindow : Window {
         BackgroundWorker _bw = new BackgroundWorker();
+        public Boolean Restart = false;
 
         public MainWindow() {
             InitializeComponent();
             _bw.DoWork += bw_DoWork;
-            _bw.RunWorkerAsync();
+            _bw.RunWorkerAsync(this);
+
         }
 
-        static async void bw_DoWork(object sender, DoWorkEventArgs e) {
+        async void bw_DoWork(object sender, DoWorkEventArgs e) {
             do {
                 try {
-
                     using (var mgr = new UpdateManager(@"C:\dev\helloworld\HelloWorld\Releases", "HelloWorldSquirrel")) {
-                        await mgr.UpdateApp();
-                    }
-                } catch (Exception) {
+                        var updateInfo = await mgr.CheckForUpdate(false, progress => { });
+                        if (updateInfo == null || updateInfo.ReleasesToApply.Count == 0)
+                            return;
 
+                        await mgr.UpdateApp();
+                        Restart = true;
+                    }
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.StackTrace);
                 }
                 Console.WriteLine("I am running");
                 Thread.Sleep(2000);
             } while (true);
         }
+
+        public void Restart_App() {
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+        }
         //           
         private void Button_Click(object sender, RoutedEventArgs e) {
-            MessageBox.Show("I updated!");
-
+            if (Restart) {
+                Restart_App();
+            }
+            MessageBox.Show("I run 2! - " + Restart);
         }
     }
 }
